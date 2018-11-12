b4f76d17083a34a27fb2c14dfd5ab1cdd5739043
shaofengrong4172 <15001124172@163.com>
2018-11-12 15:41:18 +0800

    初始化

---
+++ scripts/test_contact.py
@@ -0,0 +1,20 @@
+import time
+import pytest
+
+class TestContact:
+
+    def test_01(self):
+        print("hello1")
+        assert 1
+
+    def test_02(self):
+        print("hello2")
+        assert 1
+
+    def test_03(self):
+        print("hello3")
+        assert 1
+
+    def test_04(self):
+        print("hello4")
+        assert 0
